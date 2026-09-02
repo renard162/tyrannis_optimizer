555f3404dd3575278010507aacde5592e7d733ed
Samuel Oliveira <renard162@gmail.com>
2026-09-02 17:06:19 -0300

    Fixed dictionary with parallel backend result

--- src/tyrannis/algorithm/base.py
+++ src/tyrannis/algorithm/base.py
@@ -142,10 +142,11 @@ class ParticleBase(ABC):
         self._candidate_variables = None
         self._candidate_fitness = None
 
-    def get_result_data(self):
+    def get_result_data(self) -> dict[str, str | float | dict[str, float]] | None:
         if self._fitness is None:
             return
         return {
+            "identifier": self._identifier,
             "fitness": float(self._fitness),
             "variables": {key: float(value) for key, value in self._variables.items()},
         }
